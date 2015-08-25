2372ddbeec8357fbc06b85698f87ec239c80c054
Viengo <viengooleg@gmail.com>
2015-08-25 14:39:47 +0300

    tariffs.html

--- client/js/index.js
+++ client/js/index.js
@@ -1,19 +1,47 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $("select").select2({
         minimumResultsForSearch: Infinity,
         width: 'resolve'
     });
 
-    $('.slow-scroll a[href*=#]').on('click', function(event){
-        event.preventDefault();
-        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 300);
+//Slow scroll
+    var jump = function (e) {
+        if (e) {
+            e.preventDefault();
+            var target = $(this).attr("href");
+        } else {
+            var target = location.hash;
+        }
+
+        $('html,body').animate(
+            {
+                scrollTop: $(target).offset().top
+            }, 300, function () {
+                location.hash = target;
+            });
+
+    };
+
+    $('html, body').hide();
+
+    $(document).ready(function () {
+        $('.slow-scroll a[href^=#]').bind("click", jump);
+
+        if (location.hash) {
+            setTimeout(function () {
+                $('html, body').scrollTop(0).show();
+                jump();
+            }, 0);
+        } else {
+            $('html, body').show();
+        }
     });
 });
-
-$(function() {
-    $('.navbar-nav').on('click', function(){
-        if($('.navbar-header .navbar-toggle').css('display') !='none'){
-            $(".navbar-header .navbar-toggle").trigger( "click" );
+//Navbar auto-hide on click
+$(function () {
+    $('.navbar-nav').on('click', function () {
+        if ($('.navbar-header .navbar-toggle').css('display') != 'none') {
+            $(".navbar-header .navbar-toggle").trigger("click");
         }
     });
 });
